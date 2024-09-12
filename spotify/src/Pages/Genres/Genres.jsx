d90e0c7bb0491db3418f23758fe8b3ee0ade3a34
meirbelo <meir1.belaich@epitech.eu>
2024-09-12 18:53:03 +0100

    add genre page

--- spotify/src/Pages/Genres/Genres.jsx
+++ spotify/src/Pages/Genres/Genres.jsx
@@ -1,7 +1,106 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-function Genres() {
-  return <h1>{"Mes Genres"}</h1>;
-}
+const Genres = () => {
+
+  const [genres, setGenres] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(10);
+  const [hasMore, setHasMore] = useState(true); // Ajout pour gérer si plus d'artistes existent
+
+  const fetchGenres = async (page, limit) => {
+    try {
+      setLoading(true);
+      setError(null); // Reset l'erreur à chaque nouveau fetch
+      const response = await fetch(`http://localhost:2222/genres?page=${page}&limit=${limit}`);
+      
+      if (!response.ok) {
+        throw new Error('Erreur lors de la récupération des Artistes');
+      }
+
+      const data = await response.json();
+
+      if (data.length < limit) {
+        setHasMore(false); // Si le nombre d'artistes retournés est inférieur à la limite, plus de pages
+      } else {
+        setHasMore(true); // Si on récupère exactement la limite, il pourrait y avoir plus
+      }
+
+      setGenres(data);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchGenres(page, limit);
+  }, [page, limit]);
+
+  const handlePreviousPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (hasMore) {
+      setPage(page + 1);
+    }
+  };
+
+  const handleLimitChange = (event) => {
+    setLimit(Number(event.target.value));
+    setPage(1); // Reset à la première page lorsqu'on change la limite
+  };
+
+  if (loading) {
+    return <p>Chargement...</p>;
+  }
+
+  if (error) {
+    return <p>Erreur : {error}</p>;
+  }
+
+  if (genres.length === 0) {
+    return <p>Aucun Genres trouvé.</p>;
+  }
+
+  return (
+    <div>
+      <h2>Liste des Genres</h2>
+
+      <label>
+        Artistes par page :
+        <select value={limit} onChange={handleLimitChange}>
+          <option value={10}>10</option>
+          <option value={20}>20</option>
+          <option value={30}>30</option>
+          <option value={50}>50</option>
+        </select>
+      </label>
+
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre.id}>
+            <h3>{genre.name}</h3>
+          </li>
+        ))}
+      </ul>
+
+      <div>
+        <button onClick={handlePreviousPage} disabled={page === 1}>
+          Précédent
+        </button>
+        <span> Page {page} </span>
+        <button onClick={handleNextPage} disabled={!hasMore}>
+          Suivant
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Genres;
